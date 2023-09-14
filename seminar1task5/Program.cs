ed3e8006eac1e10c61f4643ffd23b2325eac1b78
Вова <Saharokkrsk@gmail.com>
2023-09-14 21:30:15 +0700

    ЖЗ

---
+++ seminar1task5/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿Console.Write("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 % 2 == 0)
+{
+    Console.WriteLine($"число {number2} чётное");  
+}
+else
+{
+    Console.WriteLine($"число {number2} нечётное");  
+};
---
+++ seminar1task8/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿Console.Write("Введите число: ");
+double numberN = Convert.ToDouble(Console.ReadLine());
+for (int i = 2; i <= numberN; i = i + 2)
+{
+    {
+        Console.Write($"{i} ");
+    };
+}
+Console.WriteLine();
